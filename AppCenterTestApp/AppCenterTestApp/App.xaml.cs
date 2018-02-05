aabb251ced34a4adcbfb4a8e8cb51267eaab08c2
Florian Bosshard <florian.bosshard@gmail.com>
2018-02-05 21:50:20 +0100

    Initialversion

---
+++ AppCenterTestApp/AppCenterTestApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppCenterTestApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppCenterTestAppPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=f129a1c1-5879-400d-84a6-760b26ed2e44;" +
+                            "ios=aa6177c9-4ad3-4524-8d6a-143f2bc6daa2",
+                   typeof(Analytics), typeof(Crashes));
+
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppCenterTestApp/AppCenterTestApp/AppCenterTestAppPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AppCenterTestApp
+{
+    public partial class AppCenterTestAppPage : ContentPage
+    {
+        public AppCenterTestAppPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            var y = 0;
+            var x = 1 / y;
+        }
+    }
+}
